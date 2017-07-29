327c6541bb12ea40abd20803c076bd9aa1830624
Thomas Andrejak <thomas.andrejak@gmail.com>
2017-07-29 22:59:46 +0200

    Update to Prewikka 4.0

--- idmefnav/__init__.py
+++ idmefnav/__init__.py
@@ -1,52 +1,51 @@
-import pkg_resources
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-from prewikka import database, env, utils, view, error
-from . import templates
 
-import json
+from prewikka import version, database, utils, view, error, template, mainmenu
+
 import re
 
-import graph_generator
+from .graph_generator import Schema 
+import pkg_resources
+
+class IDMEFNavParameters(mainmenu.MainMenuParameters):
+    allow_extra_parameters = True
 
 class IDMEFNav(view.View):
-    class IDMEFNavParameters(view.Parameters):
-        def register(self):
-            self.optional("idmef_class", str, default="IDMEF-Message")
-
     _HTDOCS_DIR = pkg_resources.resource_filename(__name__, 'htdocs')
 
-
     plugin_name = "IDMEFNav"
-    plugin_author = "Sélim Menouar"
-    plugin_license = "GPL"
-    plugin_version = "1.0.0"
-    plugin_copyright = "CSSI"
-    plugin_description = "IDMEF navigator"
+    plugin_author = "Sélim Ménouar, Thomas Andrejak"
+    plugin_license = version.__license__
+    plugin_version = version.__version__
+    plugin_copyright = version.__copyright__
+    plugin_description = N_("IDMEF navigator")
     plugin_htdocs = (("idmefnav", _HTDOCS_DIR),)
-    view_template = templates.idmefnav
+
     view_parameters = IDMEFNavParameters
-    view_name = N_("IDMEF")
-    view_section = N_("Help")
 
     def __init__(self):
         view.View.__init__(self)
-        self.schema = graph_generator.Schema("%s/yaml" % self._HTDOCS_DIR)
+        self.schema = Schema("%s/yaml" % self._HTDOCS_DIR)
 
+    @view.route("/help/idmefnav", menu=(N_("Help"), N_("IDMEF")))
     def render(self):
-        idmef_class = self.parameters["idmef_class"]
+        idmef_class = env.request.parameters.get("idmef_class", "IDMEF-Message")
+        dataset = {}
 
         if idmef_class not in self.schema:
-            raise error.PrewikkaUserError("Parameter Error", _("%s is not a valid IDMEF class") % idmef_class)
+            raise error.PrewikkaUserError(N_("Parameter Error"), N_("%(idmefclass)s is not a valid IDMEF class", {'idmefclass':idmef_class}))
 
-        self.dataset["schema"] = self.schema[idmef_class]
-        self.dataset["full_schema"] = self.schema
-        self.dataset["link"] = self.view_path
+        dataset["schema"] = self.schema[idmef_class]
+        dataset["full_schema"] = self.schema
+        dataset["link"] = self.view_path
 
         with open("%s/graph/%s.svg" % (self._HTDOCS_DIR, idmef_class), 'r') as stream:
-            self.dataset["svg"] = re.sub(r"\d+pt", "100%", stream.read())
+            dataset["svg"] = re.sub(r"\d+pt", "100%", stream.read())
 
         with open("%s/graph/%s" % (self._HTDOCS_DIR, idmef_class), 'r') as stream:
-            self.dataset["dot"] = stream.read()
+            dataset["dot"] = stream.read()
 
-        self.dataset["png"] = "idmefnav/graph/%s.png" % idmef_class
+        dataset["png"] = "idmefnav/graph/%s.png" % idmef_class
 
+        return template.PrewikkaTemplate(__name__, "templates/idmefnav.mak").render(**dataset)
